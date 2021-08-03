819c323f165b4188a9d4db9b26f4d41659554563
Giorgio Mazzei <43831353+ArtyMoo@users.noreply.github.com>
2021-08-03 21:46:32 +0200

    port 3000 to 5000

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello Google!</h1>');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening to port ${port}`);
 });
